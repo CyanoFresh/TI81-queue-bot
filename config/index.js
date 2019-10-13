308bdaa65afed429342d9d4a0965d91b797f450c
Alex Solomaha <cyanofresh@gmail.com>
2019-10-13 19:30:47 +0300

    ignore chance is 15% now

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ module.exports = {
   admin_chatids: process.env.ADMIN_CHATIDS.split(','),
   users: require('./users'),
   dbPath: process.env.DB_FILE_PATH || './db.json',
-  ignoreChance: 20,   // 0 to disable
+  ignoreChance: 15,   // 0 to disable
   enableStickers: true,
   webhook: {
     port: process.env.WEBHOOKS_PORT,
